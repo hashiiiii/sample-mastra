fb7e884f0e7346fd8b662959431503ea391fded3
hashiiiii <pkj59wwj@gmail.com>
2025-03-23 16:11:03 +0900

    test

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,12 +1,35 @@
-import { Mastra } from '@mastra/core/mastra';
-import { createLogger } from '@mastra/core/logger';
-
-import { weatherAgent } from './agents';
+import { Mastra } from "@mastra/core/mastra";
+import { createLogger } from "@mastra/core/logger";
+import { cursorRulesWorkflow } from "./workflows";
+import { cursorRulesAgent } from "./agents";
 
 export const mastra = new Mastra({
-  agents: { weatherAgent },
-  logger: createLogger({
-    name: 'Mastra',
-    level: 'info',
-  }),
+    agents: {
+        cursorRulesAgent,
+    },
+    workflows: {
+        cursorRulesWorkflow,
+    },
+    logger: createLogger({
+        name: "GitHub Cursor Rules Agent",
+        level: "info",
+    }),
 });
+
+// (async () => {
+//     const { runId, start } = mastra
+//         .getWorkflow("cursorRulesWorkflow")
+//         .createRun();
+
+//     console.log("Run", runId);
+
+//     const runResult = await start({
+//         triggerData: {
+//             repositoryUrl: "https://github.com/honojs/hono",
+//             branch: "main",
+//             outputPath: "./examples/rules",
+//         },
+//     });
+
+//     console.log("Final output:", runResult.results);
+// })();
